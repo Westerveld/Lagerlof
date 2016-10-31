dc2221d73e13899432aacc103e4328ce24e6d25c
Iseris <Phanlon1992@gmail.com>
2016-10-31 16:46:10 +0000

    Ph 1st commit

---
+++ Lagerloaf/Assets/Scripts/CameraController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Camera.main.transform.position = new Vector3(Camera.main.transform.position.x, Camera.main.transform.position.y + GameManager.gameSpeed * Time.deltaTime, Camera.main.transform.position.z);
+    }
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        print("Triger " + coll.gameObject.name);
+        Destroy(coll.gameObject);
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        print("Triger !!!!!!!!!!" + other.gameObject.name);
+        Destroy(other.gameObject);
+    }
+
+}
---
+++ Lagerloaf/Assets/Scripts/GameManager.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GameManager : MonoBehaviour {
+
+    public GameObject[] platformPrefabs;
+    public static float gameSpeed = 1;
+    int platformHeight = 10;
+    int level = 0;
+    
+	// Use this for initialization
+	void Start () {
+        List<GameObject> spawnedPlatforms = new List<GameObject>();
+ 
+    }
+
+    // Update is called once per frame
+    void Update () {
+     //   UpdateCameraPosition();
+        
+        if (Camera.main.transform.position.y >= level*platformHeight)
+        {
+            level++;
+            SpawnNewLevel();
+               
+        }
+        
+
+	}
+
+
+   void SpawnNewLevel()
+    {
+       
+        Instantiate(platformPrefabs[Random.Range(0, platformPrefabs.Length-1)], new Vector3(0, platformHeight * level, 0), Quaternion.identity);
+
+    }
+
+    void closeWall()
+    {
+
+    }
+
+    void SpawnWalls(Vector2 pos)
+    {
+    }
+
+}
