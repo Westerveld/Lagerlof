23f8e60eef0c7fbc70884549f7eed7f85825bee0
Julzso23 <julzso23@gmail.com>
2016-11-01 12:33:54 +0000

    Added pickup controller

---
+++ Lagerloaf/Assets/Scripts/PickupController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PickupController : MonoBehaviour
+{
+    private enum Pickups
+    {
+        Butter,
+        Lager,
+        Beer,
+        Count
+    }
+
+    Pickups pickup;
+
+    public string texturesPath;
+
+    public string Type
+    {
+        set
+        {
+            pickup = (Pickups)Pickups.Parse(typeof(Pickups), value);
+        }
+
+        get
+        {
+            return pickup.ToString();
+        }
+    }
+    
+	void Start ()
+    {
+        pickup = (Pickups)Random.Range(0, (float)Pickups.Count);
+        GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>(texturesPath + "/" + Type);
+	}
+}
