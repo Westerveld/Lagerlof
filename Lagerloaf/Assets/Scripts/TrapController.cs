356508f2f6f8a022fb9078ea9b8789d51712b10d
Julzso23 <julzso23@gmail.com>
2016-11-01 16:27:28 +0000

    Added trap prefab

---
+++ Lagerloaf/Assets/Scripts/TrapController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TrapController : MonoBehaviour
+{
+    public Items item;
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.gameObject.tag == "Player")
+        {
+            collider.gameObject.GetComponent<PlayerController>().SetSlow();
+            Destroy(gameObject);
+        }
+    }
+}
